ad7ec1aa1e689375c80faf8eafbf3921a4bd4390
John MSI <perseo1326@gmail.com>
2024-06-23 01:54:22 +0200

    initial data for visual testing

---
+++ res/datos.js
@@ -0,0 +1,63 @@
+
+
+// *********************************************************
+// VARIABLES Y CONSTANTES USADAS
+
+
+    const datos = [
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+        [ "320900", "00472962", "BOLLSTANÄS so 3 +chaiselongue/SKIFTEBO gris oscuro", 7.7, 6.6, 5, 4, 0.9, 6, 2, 28, 7, 5, 0, 1, 1002, "Tops de AOS para cubrir con más cariño", 14, 1.2, "0900" ],
+    ];
+
+
+    const cabeceras = [
+        "LV Venta",
+        "Referencia",
+        "Nombre",
+        "V.Max",
+        "VMS",
+        "Ult Sem",
+        "Semana",
+        "Sem Stock",
+        "EOQ",
+        "Pallet Qty",
+        "Stock ESBO",
+        "Stock Tienda",
+        "Pall SGF Tienda",
+        "Reservas",
+        "Pedir",
+        "Prox Camión",
+        "Obs Especiales",
+        "Pall ESBO",
+        "EOQ %",
+        "Cabeceras"
+    ];
