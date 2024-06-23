601f74e233750b69e0e6f1ed6e2c481072c250dd
John MSI <perseo1326@gmail.com>
2024-06-23 14:29:25 +0200

    initial config data

--- res/app.js
+++ res/app.js
@@ -1,6 +1,9 @@
 
 'use strict';
 
+    // EXCEL constantes
+    /* const FILE_MYME_TYPE_ARRAY = ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]; */
+
 
 // *********************************************************
 // VARIABLES AND CONSTANTS
@@ -24,14 +27,9 @@ const tableData = document.getElementById("table-data");
 
 
 
-
+const configDataURL = "./res/configData.json";
 const teclas = ["ArrowDown", "ArrowUp", "PageDown", "PageUp"];
 
-// Excel shipments file manipulation values
-const FILE_EXTENSION_ARRAY = [ "xlsx", "xlsm" ];
-const FILE_WORKBOOK_SHEET = "SDS0001";
-// const FILE_MYME_TYPE_ARRAY = ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"];
-const FILE_MYME_TYPE_ARRAY = ["application/vnd.ms-excel.sheet.macroenabled.12"];
 
 
 // *********************************************************
@@ -42,89 +40,89 @@ auxPanel.addEventListener("wheel", ( evento ) => {
     evento.preventDefault();
 });
 
+
 document.addEventListener("keydown", ( evento ) => {
     if( teclas.includes(evento.key) && auxPanel.hidden === false){
         console.log("Key down");
+        // TODO: revisar el comportamiento
         // evento.preventDefault();
     }
 });
 
+
 table.addEventListener("keydown", () => {
     console.log("EVENTO TABLE Keydown: ");
 });
+
 
 cancelButton.addEventListener("click", () => {
     // auxPanel.classList.add("no-visible");
     auxPanel.hidden = true;
 });
 
+
 sds0001Button.addEventListener("change", loadSDS0001File);
 
 loadReportsB.addEventListener("click", ProcessReports );
 
 // *********************************************************
+// *********************************************************
+
+initialize();
 
 // *********************************************************
-// Function to read a selected file
-function loadSDS0001File( evento ) {
+function initialize() {
+    console.log("Procediendo a cargar datos de configuración.")
 
+    fetch( configDataURL )
+    .then((response) => response.json())
+    .then((data) => {
+        // Aquí puedes trabajar con el objeto JavaScript resultante
+        console.log("JSON DATA: ", data);
+    })
+    .catch((error) => {
+        // const jsonObject = JSON.parse(jsonString);
+        console.log("Error procesando Datos de configuración inicial.");
+    });
+
+
+
+}
+// *********************************************************
+// *********************************************************
+// Function to read 'SDS0001' Report selected file
+function loadSDS0001File ( evento ) {
     const file = evento.target.files[0];
     auxPanel.classList.remove("no-visible");
-
     const filePointer = new ExcelFileOpen(file, FILE_EXTENSION_ARRAY, FILE_WORKBOOK_SHEET, FILE_MYME_TYPE_ARRAY );
-    
-    console.log("FILE: ", filePointer.file );
+    // console.log("FILE: ", filePointer.file );
 
-    showFileNameReport( "sds0001-file-name" , filePointer.file.name);
+    showFileNameReport( this.id + "-file-name" , filePointer.file.name);
 
-    let x = loadExcelReport( filePointer );
-
-    console.log("Valor de X: ", x);
-
-
-
-}
-
-// *********************************************************
-// function to load any excel file
-function loadExcelReport ( filePointer, myContenido ) {
     const promiseData = loadExcelFile(filePointer);
-
-    // promiseData.then( (response) => {
-        
+    promiseData.then( (response) => {
         // let contentData = validateShipmentsFile( response );
-        // console.log("Carga \"" + filePointer.file.name + "\" Finalizada!", response); 
-
-    //     contentData = getShipmentsInfoFromGrossData( contentData );
-
-    //     contentData = sortTrucksInfo(contentData);
-
-    //     document.getElementById("button-load-shipments").classList.add("no-visible");
-    //     document.getElementById("shipments-container").classList.remove("no-visible");
-    //     document.getElementById("shipments-commands").classList.remove("no-visible");
-    //     document.getElementById("box").classList.remove("box");
-
-    //     shipmentsData = arrayToMap( contentData );
-
-    //     showShipmentsData(shipmentsData);
-    //     myContenido = response;
-    // })
-    // .catch( (error) => {
-    //     console.log("ERROR:openFile: ", error);
-    //     alert(error.message);
+        console.log("Carga \"" + filePointer.file.name + "\" Finalizada!", response); 
+        // SDS0001 = response;
+    })
+    .catch( (error) => {
+        console.log("ERROR:loadSDS0001File: ", error);
+        alert(error.message);
+        // TODO: inicializar la variable del contenido para evitar errores
     //     // initializePage();
-    // })
-    // .finally( () => {
-    //     auxPanel.classList.add("no-visible");
-    // });
-    
-    return promiseData;
+    })
+    .finally( () => {
+        auxPanel.classList.add("no-visible");
+    });
 }
+
+
 
 // *********************************************************
 function showFileNameReport  ( idElement, text ) {
     document.getElementById(idElement).innerText = text;
 }
+
 
 // *********************************************************
 function ProcessReports() {
@@ -133,5 +131,6 @@ function ProcessReports() {
     console.log("ProcessReports function!");
 
 }
+
 
 // *********************************************************
