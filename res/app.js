a74a0d644cdf29074cd51b0a48116836ec712501
John IK <johnIK@gmail.com>
2024-11-12 11:55:39 +0100

    commit for publishing (production)

--- res/app.js
+++ res/app.js
@@ -165,7 +165,7 @@ let dataObs;
 // data from 'Pedido anterior ESBO' report 
 let dataPreviousOrder;
 
-const version = "3.0";
+const version = "3.1";
 
 const configDataURL = "./res/configData.json";
 const teclas = ["ArrowDown", "ArrowUp", "PageDown", "PageUp"];
